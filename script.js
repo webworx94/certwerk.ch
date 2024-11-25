08b067e01c68254d93f1efd07b0b18c0ddc54c1d
certwerk <office@certwerk.ch>
2024-11-25 08:47:38 +0100

    JavaScript File Added

---
+++ script.js
@@ -0,0 +1,81 @@
+// MOBILE NAV
+    document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navbarUl = document.querySelector('.navbar ul');
+    const menuIcon = document.querySelector('.menu-toggle img.menu-icon');
+    const closeIcon = document.querySelector('.menu-toggle img.close-icon');
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    menuToggle.addEventListener('click', function() {
+        navbarUl.classList.toggle('open'); // Toggle-Klasse für das Öffnen und Schließen des Menüs
+        
+        if (menuIcon.classList.contains('hidden')) {
+            menuIcon.src = 'images/menu.png';
+            closeIcon.src = 'images/cancel.png';
+            menuIcon.classList.remove('hidden');
+            closeIcon.classList.add('hidden');
+        } else {
+            menuIcon.src = 'images/cancel.png';
+            closeIcon.src = 'images/menu.png';
+            menuIcon.classList.add('hidden');
+            closeIcon.classList.remove('hidden');
+        }
+
+        menuIcon.classList.toggle('rotate');
+        closeIcon.classList.toggle('rotate');
+
+        // Schließe alle Dropdowns, wenn das Menü geöffnet oder geschlossen wird
+        dropdowns.forEach(dropdown => {
+            dropdown.style.display = 'none';
+        });
+    });
+
+    // Event Listener für Klicks auf die Hauptlinks "Sicherheitsschulungen" und "ISO-Zertifizierungen"
+    const mainLinks = document.querySelectorAll('.navbar ul > li > a:not(.dropdown)');
+    mainLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            // Schließe alle Dropdowns, wenn auf einen Hauptlink geklickt wird
+            dropdowns.forEach(dropdown => {
+                dropdown.style.display = 'none';
+            });
+        });
+    });
+
+    // Event Listener für Mouseenter und Mouseleave für Hauptlinks und Dropdown-Links
+    const navLinks = document.querySelectorAll('.navbar ul > li > a');
+    function changeColorToLogorot() {
+        navLinks.forEach(link => {
+            if (link.classList.contains('active')) {
+                link.style.color = 'var(--logorot)';
+            } else {
+                link.style.color = 'var(--logoblau)';
+            }
+        });
+    }
+
+    navLinks.forEach(link => {
+        link.addEventListener('mouseenter', function() {
+            link.classList.add('active');
+            changeColorToLogorot();
+        });
+
+        link.addEventListener('mouseleave', function() {
+            link.classList.remove('active');
+            changeColorToLogorot();
+        });
+
+        const dropdown = link.nextElementSibling;
+        if (dropdown && dropdown.classList.contains('dropdown')) {
+            dropdown.addEventListener('mouseenter', function() {
+                link.classList.add('active');
+                changeColorToLogorot();
+            });
+            dropdown.addEventListener('mouseleave', function() {
+                link.classList.remove('active');
+                changeColorToLogorot();
+            });
+        }
+    });
+
+    changeColorToLogorot(); // Setze die Farbe der Hauptlinks beim Laden der Seite
+});
